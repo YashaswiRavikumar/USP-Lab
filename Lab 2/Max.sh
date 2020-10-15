56296d081488519ea7fc621e44c716a27fd53251
Yashaswi R <58076484+YashaswiRavikumar@users.noreply.github.com>
2020-10-15 13:42:47 +0530

    Update Max.sh

--- Lab 2/Max.sh
+++ Lab 2/Max.sh
@@ -8,8 +8,8 @@ if [ $a -eq $b -a $a -eq $c ]; then
     echo "All the three numbers are equal"
     elif [ $a -gt $b -a $a -gt $c ]; then
         echo "$a is biggest number"
-        elif [ $b -gt $a -a $b -gt $c ]; then
-                echo "$b is biggest number"
-                elif [ $c -gt $a -a $c -gt $b ]; then
-                        echo "$c is biggest number"
+    elif [ $b -gt $a -a $b -gt $c ]; then   
+        echo "$b is biggest number"
+    else 
+        echo "$c is biggest number"
 fi
